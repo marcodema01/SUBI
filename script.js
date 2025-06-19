b35a0c3c0d57d72e4f9a6699632ea5c1d3439f48
Marco De Martino <marco.demartino@student.supsi.ch>
2025-06-19 11:50:51 +0200

    Inserito production url

--- script.js
+++ script.js
@@ -25,7 +25,8 @@ document.getElementById("btn3").addEventListener("click", () => {
 });
 
 function aggiornaDato(id) {
-  fetch("http://localhost:3001/update-character", {
+  // fetch("http://localhost:3001/update-character", {
+  fetch("https://subi-ajng.onrender.com/update-character", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ id }),
@@ -33,7 +34,8 @@ function aggiornaDato(id) {
     .then((res) => res.json())
     .then((data) => {
       console.log("Risposta update:", data);
-      return fetch("http://localhost:3001/characters");
+      // return fetch("http://localhost:3001/characters");
+      return fetch("https://subi-ajng.onrender.com/characters");
     })
     .then((res) => res.json())
     .then((data) => {
